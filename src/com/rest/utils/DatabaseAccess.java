3807a0caf65d90404e4226b7eec67d774db3d7f3
shommzz <shommzz@gmail.com>
2013-11-12 21:57:00 +0400

    added missing sql strings

--- src/com/rest/utils/DatabaseAccess.java
+++ src/com/rest/utils/DatabaseAccess.java
@@ -15,59 +15,63 @@ import com.rest.utils.exceptions.WrongEmailFormatException;
 public class DatabaseAccess implements DatabaseAccessInterface {
 	
 	//maximum allowed inputLenght according to the database ddl
-	private final static int MAX_INPUT_LENGTH = 200;
-	
-	//static Strings for SQL Queries
-	private final static String SELECT = "Select ";
-	private final static String DELETE = "Delete ";
-	private final static String UPDATE = "Update ";
-	private final static String SET = "Set ";
-	private final static String FROM = "from ";
-	private final static String WHERE = "where ";
-	private final static String DISTINCT = "distinct ";
-	private final static String INSERT_INTO = "Insert into ";
-	private final static String ALL = "* ";
-	private final static String EQUALS = " = ";
-	private final static String VALUES = "values ";
-	
-	
-	//static Strings for the table t5_user
-	private final static String USER_TABLE = "t5_users ";
-	private final static String USER_ID = "id ";
-	private final static String USER_EMAIL = "email ";
-	private final static String USER_PASSWORD = "passwd ";
-	private final static String USER_FIRSTNAME = "first_name ";
-	private final static String USER_LASTNAME = "last_name ";
-	private final static String USER_LOGINKEY = "login_key ";
-	private final static String USER_LOGIN_TIMESTAMP = "login_timestamp ";
-	private final static String USER_LAST_LOGIN = "last_login ";
-	private final static String USER_LAST_REQUEST = "last_request ";
-	private final static String USER_LOGOUT_SESSION_TIME = "logout_session_time ";
-	private final static String USER_GPS_PUSH_TIME = "gps_push_time ";
-	private final static String USER_DATED = "dated ";
-	
-	//static Strings for the table t5_locations
-	private final static String LOCATIONS_TABLE = "t5_locations ";
-	private final static String LOCATIONS_ID = "id ";
-	private final static String LOCATIONS_FSQUARE_VENUE_ID = "foursquare_venue_id ";
-	private final static String LOCATIONS_DATED = "dated ";
-	
-	//static Stings for the table t5_users_reviews
-	private static final String REVIEWS_TABLE = "t5_reviews ";
-	private static final String REVIEWS_USER_ID = "users_id ";
-	private static final String REVIEWS_LOCATION_ID = "locations_id ";
-	private static final String REVIEWS_RATING = "rating ";
-	private static final String REVIEWS_REVIEW_TITLE = "review_title ";
-	private static final String REVIEWS_REVIEW_DESCRIPTION = "review_description ";
-	private static final String REVIEWS_REVIEW_PICTURE = "review_picture ";
-	private static final String REVIEWS_DATED = "dated ";
-	
-	//static Strings for the table t5_checkins
-	private static final String CHECKIN_TABLE = "t5_checkins ";
-	private static final String CHECKIN_USER_ID = "users_id ";
-	private static final String CHECKIN_LOCATION_ID = "locations_id ";
-	private static final String CHECKIN_CHECKIN_TIMESTAMP = "checkin_timestamp ";
-	
+		private final static int MAX_INPUT_LENGTH = 200;
+		
+		//static Strings for SQL Queries
+		private final static String SELECT = "Select ";
+		private final static String DELETE = "Delete ";
+		private final static String UPDATE = "Update ";
+		private final static String SET = "Set ";
+		private final static String FROM = "from ";
+		private final static String WHERE = "where ";
+		private final static String AND = "and ";
+		private final static String DISTINCT = "distinct ";
+		private final static String INSERT_INTO = "Insert into ";
+		private final static String INSERT_IGNORE_INTO = "Insert Ignore into ";
+		private final static String ALL = "* ";
+		private final static String EQUALS = " = ";
+		private final static String VALUES = "values ";
+		
+		
+		//static Strings for the table t5_user
+		private final static String USER_TABLE = "t5_users ";
+		private final static String USER_ID = "id ";
+		private final static String USER_EMAIL = "email ";
+		private final static String USER_PASSWORD = "passwd ";
+		private final static String USER_FIRSTNAME = "first_name ";
+		private final static String USER_LASTNAME = "last_name ";
+		private final static String USER_LOGINKEY = "login_key ";
+		private final static String USER_LOGIN_TIMESTAMP = "login_timestamp ";
+		private final static String USER_LAST_LOGIN = "last_login ";
+		private final static String USER_LAST_REQUEST = "last_request ";
+		private final static String USER_LOGOUT_SESSION_TIME = "logout_session_time ";
+		private final static String USER_GEO_PUSH_INTERVAL = "geo_push_interval ";
+		private final static String USER_MIN_DISTANCE = "min_distance ";
+		private final static String USER_MAX_LOGIN_INTERVAL = "max_login_interval ";
+		private final static String USER_DATED = "dated ";
+		
+		//static Strings for the table t5_locations
+		private final static String LOCATIONS_TABLE = "t5_locations ";
+		private final static String LOCATIONS_ID = "id ";
+		private final static String LOCATIONS_FSQUARE_VENUE_ID = "foursquare_venue_id ";
+		private final static String LOCATIONS_DATED = "dated ";
+		
+		//static Stings for the table t5_users_reviews
+		private static final String REVIEWS_TABLE = "t5_reviews ";
+		private static final String REVIEWS_USER_ID = "users_id ";
+		private static final String REVIEWS_LOCATION_ID = "locations_id ";
+		private static final String REVIEWS_RATING = "rating ";
+		private static final String REVIEWS_REVIEW_TITLE = "review_title ";
+		private static final String REVIEWS_REVIEW_DESCRIPTION = "review_description ";
+		private static final String REVIEWS_REVIEW_PICTURE = "review_picture ";
+		private static final String REVIEWS_DATED = "dated ";
+		
+		//static Strings for the table t5_checkins
+		private static final String CHECKIN_TABLE = "t5_checkins ";
+		private static final String CHECKIN_USER_ID = "users_id ";
+		private static final String CHECKIN_LOCATION_ID = "locations_id ";
+		private static final String CHECKIN_CHECKIN_TIMESTAMP = "checkin_timestamp ";
+		
 	
 	@Override
 	public UserData registerNewUser(String email, String password, 
