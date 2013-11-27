c367fd295a5d67e086282b51b985b723dba008c2
Pascal Lightning <--->
2013-11-27 22:58:41 +0000

    implemented getReviewsForUser

--- src/com/rest/location/service/LocationServiceInterface.java
+++ src/com/rest/location/service/LocationServiceInterface.java
@@ -5,6 +5,7 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -69,6 +70,12 @@ public interface LocationServiceInterface {
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)	
     public Response sendReview(@FormParam ("key") String key, @FormParam("reviewId") String reviewId,  @FormParam("comment") String comment);
+	
+	@GET                                
+	@Path("reviews-for-user/{userId}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)	
+    public Response getReviewsForUser(@PathParam ("userId") int userId);
 
 }
 
