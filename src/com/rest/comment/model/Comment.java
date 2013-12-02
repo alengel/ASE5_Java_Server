6b88ae05f5d8638c31448b725f33a9734a44c7f5
shommzz <shommzz@gmail.com>
2013-12-03 00:55:58 +0400

    added equals for comment class

--- src/com/rest/comment/model/Comment.java
+++ src/com/rest/comment/model/Comment.java
@@ -16,6 +16,43 @@ public class Comment {
 	private String message;
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cd == null) ? 0 : cd.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((success == null) ? 0 : success.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (cd == null) {
+			if (other.cd != null)
+				return false;
+		} else if (!cd.equals(other.cd))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (success == null) {
+			if (other.success != null)
+				return false;
+		} else if (!success.equals(other.success))
+			return false;
+		return true;
+	}
+
 	public Comment() {
     	
     }
